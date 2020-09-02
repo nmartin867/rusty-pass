773b45f956eb43738f6cb523f481cb1c6fb0fd73
Nick Martin <nmartin867@gmail.com>
2020-09-01 23:11:04 -0600

    Begin command arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+#[macro_use]
+extern crate clap;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = clap_app!(rusty_pass =>
+        (version: "1.0")
+        (author: "Nick M. <nmartin867@gmail.com>")
+        (about: "Password Manager")
+        (@arg CREATE: -n --new +takes_value "Creates a new entry")
+        (@arg MODIFY: -m --edit +takes_value "Modifies an existing entry")
+        (@subcommand test =>
+            (about: "Manages sensitive information securely")
+            (version: "1.0")
+            (author: "Nick M. <nmartin867@gmail.com>")
+            (@arg verbose: -v --verbose "Print test information verbosely")
+        )
+    ).get_matches();
 }
